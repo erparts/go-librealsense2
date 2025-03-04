fbdf0c0e564fcae52efdaa6704b4051cec5b18d2
Maximo Cuadros <mcuadros@gmail.com>
2025-03-04 09:38:51 +0100

    handle any resolution or steam

--- pipline.go
+++ pipline.go
@@ -5,27 +5,33 @@ package librealsense2
 #cgo CPPFLAGS: -I/usr/local/include
 #include <librealsense2/rs.h>
 #include <librealsense2/h/rs_pipeline.h>
-#define STREAM          RS2_STREAM_DEPTH  // rs2_stream is a types of data provided by RealSense device           //
-#define FORMAT          RS2_FORMAT_Z16    // rs2_format identifies how binary data is encoded within a frame      //
-#define WIDTH           640               // Defines the number of columns for each frame                         //
-#define HEIGHT          480               // Defines the number of lines for each frame                           //
-#define FPS             30                // Defines the rate of frames per second                                //
-#define STREAM_INDEX    0
 */
 import "C"
 import (
 	"errors"
 	"fmt"
+	"time"
 	"unsafe"
 
 	"gocv.io/x/gocv"
 )
 
+const DefaultTimeout = time.Second * 15
+
 var (
 	ErrNotDevicesFound = errors.New("no realsense devices found")
 )
 
+type Stream C.rs2_stream
+
+const (
+	StreamDepth Stream = C.RS2_STREAM_DEPTH
+	StreamColor Stream = C.RS2_STREAM_COLOR
+)
+
 type Pipeline struct {
+	cfg *PipelineConfig
+
 	p       *C.rs2_pipeline
 	ctx     *C.rs2_context
 	conf    *C.rs2_config
@@ -33,10 +39,34 @@ type Pipeline struct {
 }
 
 type PipelineConfig struct {
-	Serial string
+	Serial        string
+	Width, Height int
+	FPS           int
+	DepthStream   bool
+	ColorStream   bool
+}
+
+func (c *PipelineConfig) Initialize() {
+	if c.Width == 0 {
+		c.Width = 640
+	}
+
+	if c.Height == 0 {
+		c.Height = 480
+	}
+
+	if c.FPS == 0 {
+		c.FPS = 30
+	}
+
+	if !c.ColorStream && !c.DepthStream {
+		c.DepthStream = true
+	}
 }
 
 func NewPipeline(cfg *PipelineConfig) (*Pipeline, error) {
+	cfg.Initialize()
+
 	var err *C.rs2_error
 	ctx := C.rs2_create_context(C.RS2_API_VERSION, &err)
 	if err != nil {
@@ -86,19 +116,37 @@ func NewPipeline(cfg *PipelineConfig) (*Pipeline, error) {
 		return nil, errorFrom(err)
 	}
 
-	C.rs2_config_enable_stream(conf, C.STREAM, C.STREAM_INDEX, C.WIDTH, C.HEIGHT, C.FORMAT, C.FPS, &err)
-
-	if err != nil {
-		return nil, errorFrom(err)
-	}
-
 	C.rs2_delete_device_list(device_list)
 
-	return &Pipeline{
+	pl := &Pipeline{
+		cfg:  cfg,
 		p:    p,
 		ctx:  ctx,
 		conf: conf,
-	}, nil
+	}
+
+	return pl, nil
+}
+
+func (pl *Pipeline) EnableStream(stream Stream, width, height, fps int) error {
+	var format C.rs2_format
+	switch stream {
+	case StreamDepth:
+		format = C.RS2_FORMAT_Z16
+	case StreamColor:
+		format = C.RS2_FORMAT_BGR8
+	default:
+		return fmt.Errorf("unknown stream %d", stream)
+	}
+
+	var err *C.rs2_error
+	C.rs2_config_enable_stream(pl.conf, C.rs2_stream(stream), 0, C.int(width), C.int(height), format, C.int(fps), &err)
+
+	if err != nil {
+		return errorFrom(err)
+	}
+
+	return nil
 }
 
 func (pl *Pipeline) Close() error {
@@ -107,6 +155,7 @@ func (pl *Pipeline) Close() error {
 	if err != nil {
 		return errorFrom(err)
 	}
+
 	C.rs2_delete_pipeline_profile(pl.profile)
 	C.rs2_delete_config(pl.conf)
 	C.rs2_delete_pipeline(pl.p)
@@ -120,40 +169,59 @@ func (pl *Pipeline) Start() error {
 	if err != nil {
 		return errorFrom(err)
 	}
+
 	pl.profile = prof
 	return nil
 }
 
-func (pl *Pipeline) WaitColorFrames(colorFrame chan *gocv.Mat) {
-	var err *C.rs2_error
+func (pl *Pipeline) WaitColorFrames(colorFrame chan *gocv.Mat, timeout time.Duration) error {
+	if timeout == 0 {
+		timeout = DefaultTimeout
+	}
+
+	var errc *C.rs2_error
 	for {
-		frames := C.rs2_pipeline_wait_for_frames(pl.p, C.RS2_DEFAULT_TIMEOUT, &err)
-		if err != nil {
-			fmt.Println(errorFrom(err))
-			continue
+		frames := C.rs2_pipeline_wait_for_frames(pl.p, C.uint(1000), &errc)
+		if errc != nil {
+			return errorFrom(errc)
 		}
 
-		count := C.rs2_embedded_frames_count(frames, &err)
-		if err != nil {
-			fmt.Println(errorFrom(err))
+		count := C.rs2_embedded_frames_count(frames, &errc)
+		if errc != nil {
+			fmt.Println(errorFrom(errc))
 			continue
 		}
 
 		for i := 0; i < int(count); i++ {
-			frame := C.rs2_extract_frame(frames, C.int(i), &err)
-			if C.rs2_is_frame_extendable_to(frame, C.RS2_EXTENSION_DEPTH_FRAME, &err) == 0 {
-				C.rs2_release_frame(frame)
+			var ret gocv.Mat
+			var err error
+
+			frame := C.rs2_extract_frame(frames, C.int(i), &errc)
+			if frame == nil {
 				continue
 			}
 
-			//fmt.Println(size)
+			w := C.rs2_get_frame_width(frame, &errc)
+			h := C.rs2_get_frame_height(frame, &errc)
 
-			rgb_frame_data := C.rs2_get_frame_data(frame, &err)
-			b := C.GoBytes(unsafe.Pointer(rgb_frame_data), 640*480*2)
+			t := gocv.MatTypeCV8UC3
+			var b []byte
+			var sz C.int
 
-			ret, errg := gocv.NewMatFromBytes(480, 640, gocv.MatTypeCV16UC1, b)
-			if errg != nil {
-				fmt.Println(errg)
+			rgb_frame_data := C.rs2_get_frame_data(frame, &errc)
+
+			if C.rs2_is_frame_extendable_to(frame, C.RS2_EXTENSION_DEPTH_FRAME, &errc) == 0 {
+				t = gocv.MatTypeCV8UC3
+				sz = w * h * 3
+			} else {
+				t = gocv.MatTypeCV16UC1
+				sz = w * h * 2
+			}
+
+			b = C.GoBytes(unsafe.Pointer(rgb_frame_data), C.int(sz))
+			ret, err = gocv.NewMatFromBytes(int(h), int(w), t, b)
+			if err != nil {
+				fmt.Println(err)
 			} else {
 				colorFrame <- &ret
 			}
