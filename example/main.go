0911379f8524c4ee5a71cccd9fd7e11d5919b733
Maximo Cuadros <mcuadros@gmail.com>
2025-03-19 03:23:40 +0100

    example: example with both streams and filters

--- example/main.go
+++ example/main.go
@@ -15,14 +15,22 @@ func main() {
 	}
 	defer pipeline.Close()
 
+	if err := pipeline.EnableStream(librealsense2.StreamDepth, 640, 480, 30); err != nil {
+		log.Fatalf("failed to enable depth stream: %v", err)
+	}
+
 	if err := pipeline.EnableStream(librealsense2.StreamColor, 640, 480, 30); err != nil {
 		log.Fatalf("failed to enable color stream: %v", err)
 	}
+
+	pipeline.SetDefaultPostProcessingFilters()
 
 	if err := pipeline.Start(); err != nil {
 		log.Fatalf("failed to start pipeline: %v", err)
 	}
 
+	windowDepth := gocv.NewWindow("Depth")
+	defer windowDepth.Close()
 	windowColor := gocv.NewWindow("Color")
 	defer windowColor.Close()
 
@@ -38,8 +46,25 @@ func main() {
 					continue
 				}
 
-				windowColor.IMShow(*frame)
-				windowColor.WaitKey(1)
+				switch frame.Type() {
+				case gocv.MatTypeCV8UC3:
+					windowColor.IMShow(*frame)
+					windowColor.WaitKey(1)
+				case gocv.MatTypeCV16UC1:
+					depthImage := gocv.NewMat()
+					frame.ConvertToWithParams(&depthImage, gocv.MatTypeCV8UC1, 255.0/5535.0, 0)
+
+					coloredDepth := gocv.NewMat()
+					gocv.ApplyColorMap(depthImage, &coloredDepth, gocv.ColormapJet)
+
+					windowDepth.IMShow(coloredDepth)
+					windowDepth.WaitKey(1)
+
+					depthImage.Close()
+					coloredDepth.Close()
+				default:
+					log.Println("unknown frame type:", frame.Type())
+				}
 
 				frame.Close()
 			}
